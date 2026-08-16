da7678b9c67f40fa471cc134e5948e5c4c570cd4
e4riya <kotkovdima0@gmail.com>
2026-08-16 21:31:28 +0300

    add ci and deploy application

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/e4riya/java-project-99/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/e4riya/java-project-99/actions)
+Result: https://task-manager-iqo7.onrender.com
--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @GetMapping(path = "/welcome")
-    public String welcome(){
-        return "Welcome to Spring";
-    }
+  @GetMapping(path = "/welcome")
+  public String welcome() {
+    return "Welcome to Spring";
+  }
 }
